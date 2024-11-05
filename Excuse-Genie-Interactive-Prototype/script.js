712ecba7687df13eeff00764008428e1af51f13f
DogDoge0-0 <arielmena167@gmail.com>
2024-11-04 19:58:04 -0500

    test 2

--- Excuse-Genie-Interactive-Prototype/script.js
+++ Excuse-Genie-Interactive-Prototype/script.js
@@ -8,17 +8,24 @@ Copy.addEventListener("click", () => {
         Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
     }
     else {
-        navigator.clipboard.writeText(AlertText.textContext).then(() => {
-            Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-check");
-            setTimeout(() => {
-                Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
-            }, 2000);
-        }).catch(err => {
+        if (AlertText.textContext == null) {
             Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
             setTimeout(() => {
                 Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
             }, 2000);
-            
+        }
+        else {
+            navigator.clipboard.writeText(AlertText.textContext).then(() => {
+                Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-check");
+                setTimeout(() => {
+                    Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
+                }, 2000);
+            }).catch(err => {
+                Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
+                setTimeout(() => {
+                    Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
+                }, 2000);
+        }   
         });
     }
 });
