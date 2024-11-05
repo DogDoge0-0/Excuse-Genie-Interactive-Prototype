39043ae52742a4e8807f9ca495a7ed05852a8083
DogDoge0-0 <arielmena167@gmail.com>
2024-11-04 20:14:40 -0500

    throw alert

--- Excuse-Genie-Interactive-Prototype/script.js
+++ Excuse-Genie-Interactive-Prototype/script.js
@@ -1,15 +1,21 @@
+// Intialization
 Alert = document.querySelector(".Alert");
 AlertText = document.querySelector("#AlertText");
 Copy = document.querySelector("#Copy");
 ExcuseOutput = document.querySelector("#ExcuseOutput");
 
+
+// Confirm that the copy works, and throw error if it won't
 Copy.addEventListener("click", () => {
     if (!navigator.clipboard) {
         Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
-    }
+        ThrowAlert("Copying to clipboard isn't supported by your browser and has been disabled");
+        Copy.classList.add("NoClick")
+    } 
     else {
         if (AlertText.textContext == null) {
             Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
+            ThrowAlert("No text to copy");
             setTimeout(() => {
                 Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
             }, 2000);
@@ -21,6 +27,7 @@ Copy.addEventListener("click", () => {
                     Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
                 }, 2000);
             }).catch(err => {
+                ThrowAlert("Error: " + err);
                 Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
                 setTimeout(() => {
                     Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
@@ -29,3 +36,16 @@ Copy.addEventListener("click", () => {
         }
     }
 });
+
+//
+ThrowAlert(string message) {
+    AlertText.textContext = message;
+    Alert.classList.add("Alert-Animation");
+    setTimeout(() => {
+        Alert.classList.replace("Alert-Animation", "Alert-Animation-Reverse");
+    }, 2000);
+    setTimeout(() => {
+        Alert.classList.remove("Alert-Animation-Reverse");
+    }, 2000);
+    AlertText.textContext = null;
+}
