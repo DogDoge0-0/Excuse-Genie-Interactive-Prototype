ade61ea15d222b085d323771bc8c73fe05e5b028
DogDoge <arielmena167@gmail.com>
2024-11-06 14:47:15 -0500

    version

--- Excuse-Genie-Interactive-Prototype/Excuse.js
+++ Excuse-Genie-Interactive-Prototype/Excuse.js
@@ -1,8 +1,4 @@
 // Intialization
-let Alert = document.querySelector(".Alert");
-let AlertText = document.querySelector("#AlertText");
-let Copy = document.querySelector("#Copy");
-let DisableButton = false;
 let ExcuseButton = document.querySelector("#ExcuseButton");
 let ExcuseOutput = document.querySelector("#ExcuseOutput");
 
@@ -132,69 +128,6 @@ let ExcuseArray = [
   "I lost my wallet.",
   "I had to help a relative with their tech issue.",
   "My calendar app didn’t sync."];
-
-// Confirm that the copy works, and throw error if it won't
-Copy.addEventListener("click", () => {
-    if (DisableButton == false) {
-        DisableButton = true;
-    }
-    else {
-        return;
-    }
-    if (!navigator.clipboard) {
-        Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
-        ThrowAlert("Copying to clipboard isn't supported by your browser and has been disabled.");
-        Copy.classList.add("NoClick");
-        setTimeout(() => {
-        }, 3000);
-    } 
-    else {
-        if (ExcuseOutput.textContent == "") {
-            Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
-            ThrowAlert("No text to copy.");
-            Copy.classList.add("NoClick");
-            setTimeout(() => {
-                Copy.classList.remove("NoClick");
-                Copy.firstElementChild.classList.replace("bi-clipboard2-x", "bi-clipboard2");
-            }, 3000);
-        }
-        else {
-            navigator.clipboard.writeText(ExcuseOutput.textContent).then(() => {
-                Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-check");
-                Copy.classList.add("NoClick")
-                ThrowAlert("Copied excuse to clipboard!");
-                setTimeout(() => {
-                    Copy.classList.remove("NoClick");
-                    Copy.firstElementChild.classList.replace("bi-clipboard2-check", "bi-clipboard2");
-                }, 3000);
-            }).catch(err => {
-                Copy.firstElementChild.classList.replace("bi-clipboard2", "bi-clipboard2-x");
-                Copy.classList.add("NoClick");
-                ThrowAlert("Error: " + err);
-                setTimeout(() => {
-                    Copy.classList.remove("NoClick");
-                    Copy.firstElementChild.classList.replace("bi-clipboard2-x", "bi-clipboard2");
-                }, 3000);
-            });   
-        }
-    }
-    setTimeout(() => {
-        DisableButton = false;
-    }, 3000);
-});
-
-// Throw Alert if something goes wrong, use animation to draw user's attention to alert
-function ThrowAlert(message) {
-    AlertText.textContent = message;
-    Alert.classList.add("Alert-Animation");
-    setTimeout(() => {
-        Alert.classList.replace("Alert-Animation", "Alert-Animation-Reverse");
-    }, 1500);
-    setTimeout(() => {
-        Alert.classList.remove("Alert-Animation-Reverse");
-        AlertText.textContent = null;
-    }, 3000);
-}
 
 // Pick an excuse at random
 ExcuseButton.addEventListener("click", () => {
---
+++ Excuse-Genie-Interactive-Prototype/Version.js
@@ -0,0 +1,9 @@
+// Initialization
+const CurrentVersion = "Alpha v0.0.1a";
+const VersionDisplay = document.createElement("p");
+
+// Set Version to display with proper styling
+VersionDisplay.textContent = CurrentVersion;
+VersionDisplay.style.position = absolute;
+VersionDisplay.style.bottom = 5vh;
+VersionDisplay.style.right = 5vw;
