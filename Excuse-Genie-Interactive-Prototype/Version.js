f5c45db34d6d1b30a7580c1559e62543355a42b4
DogDoge <arielmena167@gmail.com>
2024-11-06 15:05:57 -0500

    done

--- Excuse-Genie-Interactive-Prototype/Version.js
+++ Excuse-Genie-Interactive-Prototype/Version.js
@@ -3,14 +3,10 @@ const CurrentVersion = "Alpha v0.0.1a";
 const Display = document.createElement("p");
 const Holder = document.createElement("span");
 
-// Set VersionHolder to display with proper styling
+// Give Holder proper style and structure
 document.body.appendChild(Holder);
-Holder.style.display = "flex";
-Holder.style.justifyContent = "flex-end";
-Holder.style.position = "absolute";
-Holder.style.bottom = "10px";
-Holder.style.width = "90vw";
+Holder.classList.add("VersHolder");
 
-
+// Display Version
 Display.textContent = CurrentVersion;
 Holder.appendChild(Display);
