d59941f893a9796d41bd35c700dc8f34565b5ce6
DogDoge <arielmena167@gmail.com>
2024-11-06 14:52:05 -0500

    ?

--- Excuse-Genie-Interactive-Prototype/Version.js
+++ Excuse-Genie-Interactive-Prototype/Version.js
@@ -1,10 +1,9 @@
 // Initialization
 const CurrentVersion = "Alpha v0.0.1a";
 const VersionDisplay = document.createElement("p");
-console.log(VersionDisplay)
 
 // Set Version to display with proper styling
 VersionDisplay.textContent = CurrentVersion;
 VersionDisplay.style.position = "absolute";
-VersionDisplay.style.bottom = "5vh";
-VersionDisplay.style.right = "5vw";
+VersionDisplay.style.bottom = "50%";
+VersionDisplay.style.right = "50%";
