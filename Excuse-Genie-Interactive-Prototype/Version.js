7240a04f36cffa391ff899cd4588367e480e9b4c
DogDoge <arielmena167@gmail.com>
2024-11-06 14:58:11 -0500

    fix

--- Excuse-Genie-Interactive-Prototype/Version.js
+++ Excuse-Genie-Interactive-Prototype/Version.js
@@ -7,13 +7,13 @@ const Holder = document.createElement("span");
 document.body.appendChild(Holder);
 Holder.style.display = "flex";
 Holder.style.justifyContent = "right";
-Holder.style.position = "relative";
+Holder.style.position = "absolute";
 Holder.style.bottom = "10px";
 
 
 
 Display.textContent = CurrentVersion;
-Holder.appendChild(VersionDisplay);
+Holder.appendChild(Display);
 Display.style.position = "absolute";
 Display.style.bottom = "50vh";
 Display.style.right = "50vw";
