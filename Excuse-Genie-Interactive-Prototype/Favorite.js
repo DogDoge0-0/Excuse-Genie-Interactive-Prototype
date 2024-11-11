08c76a7ef9f0cda8a9577e2a8c232bde94b5afe6
DogDoge0-0 <arielmena167@gmail.com>
2024-11-11 18:20:44 -0500

    not done

---
+++ Excuse-Genie-Interactive-Prototype/Favorite.js
@@ -0,0 +1,20 @@
+﻿// Intialization
+let FavoriteButton = document.querySelector("#Favorite");
+let ExcuseOutput = document.querySelector("#ExcuseOutput");
+let FavoriteArray = {}
+
+FavoriteButton.addEventListener("click", () => {
+    if (ExcuseOutput.textContent) {
+        FavoriteArray.push(ExcuseOutput.textContent)
+        FavoriteButton.classList.replace("bi-bookmark", "bi-bookmark-check")
+        setTimeout(() => {
+            FavoriteButton.classList.replace("bi-bookmark-check", "bi-bookmark")
+        }, 100);
+    }
+    else {
+        FavoriteButton.classList.replace("bi-bookmark", "bi-bookmark-x")
+        setTimeout(() => {
+            FavoriteButton.classList.replace("bi-bookmark-x", "bi-bookmark")
+        }, 100);
+    }
+});
